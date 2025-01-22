b7165202ab5e97a8460e7840c067de8074643827
tong2 <976558171@qq.com>
2025-01-22 22:03:41 +0800

    导入导出示例

--- src/modules/finance/entity/orders.ts
+++ src/modules/finance/entity/orders.ts
@@ -9,292 +9,288 @@ export class FinanceOrdersEntity extends BaseEntity {
   @PrimaryGeneratedColumn({ comment: '主键ID' })
   id: number;
 
-  @Index()
-  @Column({ comment: '主订单编号', length: 50 })
-  mainOrderNumber: string;
+  @Column({ comment: '主订单编号', nullable: true, length: 50 })
+  main_order_number: string;
 
-  @Index()
-  @Column({ comment: '子订单编号', length: 50 })
-  subOrderNumber: string;
+  @Column({ comment: '子订单编号', nullable: true, length: 50 })
+  sub_order_number: string;
 
-  @Column({ comment: '选购商品', type: 'text' })
-  selectedGoods: string;
+  @Column({ comment: '选购商品', nullable: true, type: 'text' })
+  selected_goods: string;
 
-  @Column({ comment: '商品规格', length: 100 })
-  productSpecification: string;
+  @Column({ comment: '商品规格', nullable: true, length: 100 })
+  product_specification: string;
 
-  @Column({ comment: '商品数量', type: 'int' })
-  productQuantity: number;
+  @Column({ comment: '商品数量', nullable: true, type: 'int' })
+  product_quantity: number;
 
-  @Column({ comment: '商品ID', length: 50 })
-  productId: string;
+  @Column({ comment: '商品ID', nullable: true, length: 50 })
+  product_id: string;
 
-  @Column({ comment: '商家编码', length: 50 })
-  merchantCode: string;
+  @Column({ comment: '商家编码', nullable: true, length: 50 })
+  merchant_code: string;
 
   @Column({
-    comment: '商品单价',
+    comment: '商品单价', nullable: true,
     type: 'decimal',
     precision: 10,
     scale: 2,
   })
-  productPrice: number;
+  product_price: number;
 
   @Column({
-    comment: '订单应付金额',
+    comment: '订单应付金额', nullable: true,
     type: 'decimal',
     precision: 10,
     scale: 2,
   })
-  orderPayableAmount: number;
+  order_payable_amount: number;
 
   @Column({
-    comment: '运费',
-    type: 'decimal',
-    precision: 10,
-    scale: 2,
-    default: 0,
-  })
-  shippingFee: number;
-
-  @Column({
-    comment: '优惠总金额',
+    comment: '运费', nullable: true,
     type: 'decimal',
     precision: 10,
     scale: 2,
     default: 0,
   })
-  totalDiscountAmount: number;
+  shipping_fee: number;
 
   @Column({
-    comment: '平台优惠',
+    comment: '优惠总金额', nullable: true,
     type: 'decimal',
     precision: 10,
     scale: 2,
     default: 0,
   })
-  platformDiscount: number;
+  total_discount_amount: number;
 
   @Column({
-    comment: '商家优惠',
+    comment: '平台优惠', nullable: true,
     type: 'decimal',
     precision: 10,
     scale: 2,
     default: 0,
   })
-  merchantDiscount: number;
+  platform_discount: number;
 
   @Column({
-    comment: '达人优惠',
+    comment: '商家优惠', nullable: true,
     type: 'decimal',
     precision: 10,
     scale: 2,
     default: 0,
   })
-  influencerDiscount: number;
+  merchant_discount: number;
 
   @Column({
-    comment: '商家改价',
+    comment: '达人优惠', nullable: true,
     type: 'decimal',
     precision: 10,
     scale: 2,
     default: 0,
   })
-  merchantPriceAdjustment: number;
+  influencer_discount: number;
 
   @Column({
-    comment: '支付优惠',
+    comment: '商家改价', nullable: true,
     type: 'decimal',
     precision: 10,
     scale: 2,
     default: 0,
   })
-  paymentDiscount: number;
+  merchant_price_adjustment: number;
 
   @Column({
-    comment: '红包抵扣',
+    comment: '支付优惠', nullable: true,
     type: 'decimal',
     precision: 10,
     scale: 2,
     default: 0,
   })
-  redEnvelopeDeduction: number;
-
-  @Column({ comment: '支付方式', length: 50 })
-  paymentMethod: string;
+  payment_discount: number;
 
   @Column({
-    comment: '手续费',
+    comment: '红包抵扣', nullable: true,
     type: 'decimal',
     precision: 10,
     scale: 2,
     default: 0,
   })
-  transactionFee: number;
+  red_envelope_deduction: number;
 
-  @Column({ comment: '收件人', length: 100 })
-  recipientName: string;
+  @Column({ comment: '支付方式', nullable: true, length: 50 })
+  payment_method: string;
 
-  @Column({ comment: '收件人手机号', length: 20 })
-  recipientPhoneNumber: string;
+  @Column({
+    comment: '手续费', nullable: true,
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+    default: 0,
+  })
+  transaction_fee: number;
 
-  @Column({ comment: '省', length: 50 })
+  @Column({ comment: '收件人', nullable: true, length: 100 })
+  recipient_name: string;
+
+  @Column({ comment: '收件人手机号', nullable: true, length: 20 })
+  recipient_phone_number: string;
+
+  @Column({ comment: '省', nullable: true, length: 50 })
   province: string;
 
-  @Column({ comment: '市', length: 50 })
+  @Column({ comment: '市', nullable: true, length: 50 })
   city: string;
 
-  @Column({ comment: '区', length: 50 })
+  @Column({ comment: '区', nullable: true, length: 50 })
   district: string;
 
-  @Column({ comment: '街道', length: 100 })
+  @Column({ comment: '街道', nullable: true, length: 100 })
   street: string;
 
-  @Column({ comment: '详细地址', length: 255 })
-  detailedAddress: string;
+  @Column({ comment: '详细地址', nullable: true, length: 255 })
+  detailed_address: string;
 
-  @Column({ comment: '是否修改过地址', type: 'boolean', default: false })
-  isAddressModified: boolean;
+  @Column({ comment: '是否修改过地址', nullable: true, length: 50 })
+  is_address_modified: string;
 
   @Column({ comment: '买家留言', nullable: true, type: 'text' })
-  buyerMessage: string;
+  buyer_message: string;
 
-  @Column({ comment: '订单提交时间', type: 'timestamp' })
-  orderSubmissionTime: Date;
+  @Column({ comment: '订单提交时间', nullable: true, type: 'timestamp' })
+  order_submission_time: Date;
 
   @Column({ comment: '旗帜颜色', nullable: true, length: 20 })
-  flagColor: string;
+  flag_color: string;
 
   @Column({ comment: '商家备注', nullable: true, type: 'text' })
-  merchantRemark: string;
+  merchant_remark: string;
 
   @Column({ comment: '订单完成时间', nullable: true, type: 'timestamp' })
-  orderCompletionTime: Date;
+  order_completion_time: Date;
 
   @Column({ comment: '支付完成时间', nullable: true, type: 'timestamp' })
-  paymentCompletionTime: Date;
+  payment_completion_time: Date;
 
-  @Column({ comment: 'APP渠道', length: 50 })
-  appChannel: string;
+  @Column({ comment: 'APP渠道', nullable: true, length: 50 })
+  app_channel: string;
 
-  @Column({ comment: '流量来源', length: 50 })
-  trafficSource: string;
+  @Column({ comment: '流量来源', nullable: true, length: 50 })
+  traffic_source: string;
 
-  @Column({ comment: '订单状态', length: 50 })
-  orderStatus: string;
+  @Column({ comment: '订单状态', nullable: true, length: 50 })
+  order_status: string;
 
   @Column({ comment: '承诺发货时间', nullable: true, type: 'timestamp' })
-  promisedDeliveryTime: Date;
+  promised_delivery_time: Date;
 
-  @Column({ comment: '订单类型', length: 50 })
-  orderType: string;
+  @Column({ comment: '订单类型', nullable: true, length: 50 })
+  order_type: string;
 
   @Column({ comment: '鲁班落地页ID', nullable: true, length: 50 })
-  lubanPageId: string;
+  luban_page_id: string;
 
   @Column({ comment: '达人ID', nullable: true, length: 50 })
-  influencerId: string;
+  influencer_id: string;
 
   @Column({ comment: '达人昵称', nullable: true, length: 50 })
-  influencerNickname: string;
+  influencer_nickname: string;
 
   @Column({ comment: '所属门店ID', nullable: true, length: 50 })
-  storeId: string;
+  store_id: string;
 
   @Column({ comment: '售后状态', nullable: true, length: 50 })
-  afterSalesStatus: string;
+  after_sales_status: string;
 
   @Column({ comment: '取消原因', nullable: true, type: 'text' })
-  cancellationReason: string;
+  cancellation_reason: string;
 
   @Column({ comment: '预约发货时间', nullable: true, type: 'timestamp' })
-  scheduledDeliveryTime: Date;
+  scheduled_delivery_time: Date;
 
   @Column({ comment: '仓库ID', nullable: true, length: 50 })
-  warehouseId: string;
+  warehouse_id: string;
 
   @Column({ comment: '仓库名称', nullable: true, length: 100 })
-  warehouseName: string;
+  warehouse_name: string;
 
-  @Column({ comment: '是否安心购', type: 'boolean', default: false })
-  isSafePurchase: boolean;
+  @Column({ comment: '是否安心购', nullable: true, length: 100 })
+  is_safe_purchase: string;
 
   @Column({ comment: '广告渠道', nullable: true, length: 50 })
-  adChannel: string;
+  ad_channel: string;
 
   @Column({ comment: '流量类型', nullable: true, length: 50 })
-  trafficType: string;
+  traffic_type: string;
 
   @Column({ comment: '流量体裁', nullable: true, length: 50 })
-  trafficFormat: string;
+  traffic_format: string;
 
   @Column({ comment: '流量渠道', nullable: true, length: 50 })
-  trafficChannel: string;
+  traffic_channel: string;
 
   @Column({ comment: '发货主体', nullable: true, length: 100 })
-  deliveryEntity: string;
+  delivery_entity: string;
 
   @Column({ comment: '发货主体明细', nullable: true, type: 'text' })
-  deliveryEntityDetails: string;
+  delivery_entity_details: string;
 
   @Column({ comment: '发货时间', nullable: true, type: 'timestamp' })
-  deliveryTime: Date;
+  delivery_time: Date;
 
   @Column({
-    comment: '降价类优惠',
+    comment: '降价类优惠', nullable: true,
     type: 'decimal',
     precision: 10,
     scale: 2,
     default: 0,
   })
-  priceReductionDiscount: number;
+  price_reduction_discount: number;
 
   @Column({
-    comment: '平台实际承担优惠金额',
+    comment: '平台实际承担优惠金额', nullable: true,
     type: 'decimal',
     precision: 10,
     scale: 2,
     default: 0,
   })
-  platformActualDiscount: number;
+  platform_actual_discount: number;
 
   @Column({
-    comment: '商家实际承担优惠金额',
+    comment: '商家实际承担优惠金额', nullable: true,
     type: 'decimal',
     precision: 10,
     scale: 2,
     default: 0,
   })
-  merchantActualDiscount: number;
+  merchant_actual_discount: number;
 
   @Column({
-    comment: '达人实际承担优惠金额',
+    comment: '达人实际承担优惠金额', nullable: true,
     type: 'decimal',
     precision: 10,
     scale: 2,
     default: 0,
   })
-  influencerActualDiscount: number;
+  influencer_actual_discount: number;
 
   @Column({ comment: '预计送达时间', nullable: true, type: 'timestamp' })
-  estimatedDeliveryTime: Date;
+  estimated_delivery_time: Date;
 
   @Column({
-    comment: '是否平台仓自流转',
-    type: 'boolean',
-    default: false,
+    comment: '是否平台仓自流转', nullable: true, length: 50
   })
-  isPlatformWarehouseAutoTransfer: boolean;
+  is_platform_warehouse_auto_transfer: string;
 
   @Column({ comment: '车型', nullable: true, length: 50 })
-  vehicleType: string;
+  vehicle_type: string;
 
   @Column({ comment: '预约送达时间', nullable: true, type: 'timestamp' })
-  scheduledDeliveryArrivalTime: Date;
+  scheduled_delivery_arrival_time: Date;
 
   @Column({ comment: '建议发货时间（起）', nullable: true, type: 'timestamp' })
-  suggestedDeliveryStartTime: Date;
+  suggested_delivery_start_time: Date;
 
   @Column({ comment: '建议发货时间（止）', nullable: true, type: 'timestamp' })
-  suggestedDeliveryEndTime: Date;
+  suggested_delivery_end_time: Date;
 }

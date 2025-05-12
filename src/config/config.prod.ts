12e00bda6c38ac086f09ab4eec44e2974cde2805
tong2 <976558171@qq.com>
2025-05-12 20:59:14 +0800

    配置调整

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -9,9 +9,9 @@ export default {
     dataSource: {
       default: {
         type: 'mysql',
-        host: '127.0.0.1',
+        host: '111.230.97.166',
         port: 3306,
-        username: 'root',
+        username: 'cool',
         password: '123456',
         database: 'cool',
         // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
